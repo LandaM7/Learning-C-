91db0db0a91bc9d991565db013a5e4010c00eab7
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-11-13 21:49:24 +0400

    TryParse

--- learning/loops_tsiklebi/loops/Program.cs
+++ learning/loops_tsiklebi/loops/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("1");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine( "moqmedeba");
+char symbol = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("2");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+
+if (number2 == 0)
+{
+    Console.WriteLine("nulze gayofa ar SeiZleba");
+}
+else if(symbol=='+')
+{
+    Console.WriteLine(number1+number2);
+}
+else if (symbol == '-')
+{
+    Console.WriteLine(number1 - number2);
+}
+else if (symbol == '*')
+{
+    Console.WriteLine(number1 * number2);
+}
+else if (symbol == '/')
+{
+    Console.WriteLine(number1 / number2);
+}
+else if (symbol == '^')
+{
+    Console.WriteLine(number1 * number1);
+}
