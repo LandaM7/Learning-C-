54a2f8ac7efd4c46f99e969784977f40543a7f51
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-11-14 21:09:34 +0400

    loops

--- learning/TryParse/TryParse/obj/Release/net8.0/TryParse.AssemblyInfo.cs
+++ learning/TryParse/TryParse/obj/Release/net8.0/TryParse.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TryParse")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca3018977ee308e118994ca6c145455746fb3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TryParse")]
