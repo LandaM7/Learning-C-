ebe114fbc342351f34a687324100f655b5f5e925
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-11-16 12:20:31 +0400

    TryParse

--- learning/TryParse/TryParse/Program.cs
+++ learning/TryParse/TryParse/Program.cs
@@ -1,39 +1,4 @@
-﻿
-//Console.WriteLine("1");
-//double number1 = Convert.ToDouble(Console.ReadLine());
-//Console.WriteLine( "moqmedeba");
-//char symbol = Convert.ToChar(Console.ReadLine());
-//Console.WriteLine("2");
-//double number2 = Convert.ToDouble(Console.ReadLine());
-
-
-//if (number2 == 0)
-//{
-//    Console.WriteLine("nulze gayofa ar SeiZleba");   
-//}
-//else if(symbol=='+')
-//{
-//    Console.WriteLine(number1+number2);
-//}
-//else if (symbol == '-')
-//{
-//    Console.WriteLine(number1 - number2);
-//}
-//else if (symbol == '*')
-//{
-//    Console.WriteLine(number1 * number2);
-//}
-//else if (symbol == '/')
-//{
-//    Console.WriteLine(number1 / number2);
-//}
-//else if (symbol == '^')
-//{
-//    Console.WriteLine(number1 * number1);
-//}
-
-
-//კომილერის ეროირს დროს პროგრამა არც ეშვება 
+﻿//კომილერის ეროირს დროს პროგრამა არც ეშვება 
 //რანთაიმ ერორი დროს რაღაც მიუვიდა გაშვების მერე ისეთი რასაც არ ელოდებოდა 
 
 //Exception არის ყველა ექსეფშენის მშობელი კლასი, მაგ ფორმატექსეფშენი
@@ -175,3 +140,5 @@
 //{
 //    Console.WriteLine("finally");
 //}
+
+
