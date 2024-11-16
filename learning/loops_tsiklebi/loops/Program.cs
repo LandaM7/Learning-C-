d4e92e72fe1b2bd424c9912a2555d5988ea74114
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-11-16 13:39:51 +0400

    loops

--- learning/loops_tsiklebi/loops/Program.cs
+++ learning/loops_tsiklebi/loops/Program.cs
@@ -109,39 +109,6 @@
 //}   
 
 
-//Console.WriteLine("1");
-//double number1 = Convert.ToDouble(Console.ReadLine());
-//Console.WriteLine( "moqmedeba");
-//char symbol = Convert.ToChar(Console.ReadLine());
-//Console.WriteLine("2");
-//double number2 = Convert.ToDouble(Console.ReadLine());
-
-
-//if (number2 == 0)
-//{
-//    Console.WriteLine("nulze gayofa ar SeiZleba");   
-//}
-//else if(symbol=='+')
-//{
-//    Console.WriteLine(number1+number2);
-//}
-//else if (symbol == '-')
-//{
-//    Console.WriteLine(number1 - number2);
-//}
-//else if (symbol == '*')
-//{
-//    Console.WriteLine(number1 * number2);
-//}
-//else if (symbol == '/')
-//{
-//    Console.WriteLine(number1 / number2);
-//}
-//else if (symbol == '^')
-//{
-//    Console.WriteLine(number1 * number1);
-//}
-
 
 
 
