d7def8a933495b883ddc9d97d225c34abf01a319
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-11-23 21:05:38 +0400

    metodebi

---
+++ learning/loops_tsiklebi/Array/Program.cs
@@ -0,0 +1,253 @@
+﻿//string[] name = new string[3];
+//name[0] = "landa";
+//name[1] = "landa2";
+//name[2] = "landa3";
+
+//Console.WriteLine(name[2]);
+
+
+
+//string[] name = new string[5];
+//name[0] = "landa";
+//name[1] = "landa2";
+//name[2] = "landa3";
+//name[3] = "landa4";
+//name[4] = "landa5";
+
+//for (int i = 0; i < 5; i += 2)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+
+
+//string[] name = new string[3];
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//   name[i]=Console.ReadLine();
+//}
+
+//for (int i = 0; i < name.Length; i ++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+
+//string[] name = new string[3] { "landa", "landa2", "landa3" };
+
+//string[] name = new string[] { "landa", "landa2", "landa3" };
+
+//string[] name = { "landa", "landa2", "landa3" };
+
+
+//int[] number = new int[3] {2,3,4 };
+//int sum = default;
+
+//for (int i=0; i<number.Length; i++)
+//{
+//    sum += number[i];
+//}
+
+//Console.WriteLine( sum );
+
+
+
+//sum პირდაპირ პირველ ელემენტს გავუტოლე და 0 დამატებით მიმატაება აღარა მჭირდება
+// ასეთ დროს  i უკვე უნდა იყოს 1 რადგან 0 ინდექსიანი ელემენტი უკვე sum ის მნიშვნელობაა
+
+//int[] number = new int[3] { 2, 3, 4 };
+//int sum = number[0];
+
+//for (int i = 1; i < number.Length; i++)
+//{
+//    sum += number[i];
+//}
+//Console.WriteLine(sum);
+
+
+//int[] number = new int[4];
+//int sum = default;
+//int namravli;
+//for (int i = 0; i < number.Length; i++)
+//{
+//    number[i] = Convert.ToInt32(Console.ReadLine());
+//}
+//for (int i = 0; i < number.Length; i++)
+//    Console.WriteLine(number[i]);
+
+//for (int i = 1; i < number.Length; i += 2)
+//    sum += number[i];
+//Console.WriteLine(sum);
+
+//namravli = number[0];
+//for (int i = 1; i < number.Length; i++)
+//    namravli *= number[i];
+//Console.WriteLine(namravli);
+
+
+////დაგვიწერს რომ არის სტრინგების მასივი, რამდენ ელემნტიანიც არის იმდენჯერ
+//string[] name = { "test1", "test2"};
+//for (int i=0; i<name.Length; i++)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+
+//string[] name = { "test1", "test2" };
+//for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+
+//რამდენიმე ფორის შემთხვევაში 
+//\t სლეში
+
+
+//for (int i = 1; i < 4; i++)
+//{
+//    Console.WriteLine($"Cycle N1, Iteracia N{i}");
+
+//    for (int j = 1; j < 3; j++)
+//    {
+//        Console.WriteLine($"\tCycle N2, Iteracia N{j}");
+
+//        for (int z = 1; z < 3; z++)
+//        {
+//            Console.WriteLine($"\t\tCycle N3, Iteracia N{z}");
+//        }
+
+//    }
+//}
+
+
+
+//int[] numbers = { -4, 0, 4, 5, -11 };
+//int min = numbers[0];
+//int max = numbers[0];
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    if (min > numbers[i])
+//    {
+//        min = numbers[i];
+//        Console.WriteLine(min);
+//    }
+
+//  }
+
+
+
+//მინიმუმი და მაქსიმუმი ერთად
+//int[] numbers = { -4, 0, 4, 5, -11 };
+//int min = numbers[0];
+//int max = numbers[0];
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    if (numbers[i]<min)
+//    {
+//        min = numbers[i];        
+//    }
+//    else if (numbers[i]>max)
+//    {
+//        max = numbers[i];        
+//    }
+
+//}
+//Console.WriteLine($"min= {min} and max = {max}");
+
+
+//List<string> names = new List<string>();
+//names.Add("test1");
+//names.Add("test2");
+//names.Add("test3");
+
+//for(int i=0; i<names.Count; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+
+//List<string> names = new List<string>();
+
+//for (int i=0; i<2; i++)
+//{
+//    string name = Console.ReadLine();
+//    names.Add(name);
+//}
+//for (int i = 0; i < names.Count; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+
+
+//ყველა მონაცემს აიღებს name მიმდევრობით
+//List<string> names = new List<string>() { "test1", "test2" };
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+//ცარელი სტრინგით შეავსებს
+//List<string> names = new List<string>() { "test1", "test2" };
+//for (int i = 0; i < names.Count; i++)
+//{
+//    names[i] = "";
+//}
+
+
+
+//List<string> names = new List<string>()
+//{
+//    "test1", "test2" ,"test3"
+//};
+
+//Random random = new Random();
+//int i = random.Next(0, names.Count);
+//Console.WriteLine( names[i]);
+
+
+//using System.Security.Cryptography;
+
+//List<int> numbers = new List<int>();
+//Random random = new Random();
+
+//for (int i = 0; i < 10; i++)
+//{
+//    numbers.Add(random.Next(0, 101));
+//}
+
+//for (int i = 0; i < numbers.Count; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+
+//List<int> oddnumber = new List<int>();
+//List<int> evennumber = new List<int>();
+//foreach (int number in numbers)
+//{
+//    if (number % 2 == 0)
+//    {
+//        evennumber.Add(number);
+//    }
+//    else
+//    {
+//        oddnumber.Add(number);
+//    }
+//}
+
+
+//foreach (int newnumber in oddnumber) 
+//{
+//    Console.WriteLine(newnumber);
+//}
+//foreach (int newnumber in evennumber) 
+//{
+//    Console.WriteLine(newnumber);
+//}
+
+
---
+++ learning/metodebi/metodebi/Program.cs
@@ -0,0 +1,194 @@
+﻿
+//string name = "landa";
+
+////ამოწმებს რითი იწყება
+//bool a = name.StartsWith('a');
+////ამოწმებს რითი მთავრდება
+//bool b = name.EndsWith("a");
+////ამოწმებს თუ არის ეს სიმბოლო
+//bool c = name.Contains("n");
+////ამოწმებს ტოლობას
+//bool d = name.Equals("test");
+
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(d);
+
+
+////ფროფერთია 
+////ასოების რაოდენობა
+//string name = "landa";
+//int a = name.Length;
+//Console.WriteLine(a);
+
+
+//string name = "landa";
+//// დიდი ასეობი
+//string a = name.ToUpper();
+//Console.WriteLine(a);
+////პატარა ასოები
+//string b = name.ToLower();
+//Console.WriteLine(b);
+
+
+//// თავში და ბოლოში აცლის სფეისებს
+//string name = "  la nd a  ";
+//string a = name.Trim();
+//Console.WriteLine(a);
+
+
+//// ამოწმებს რომელ ინდექსზე დგას , თავიდან ითვლის 
+//// სიმბოლო თუ საერთოდ არაა აბრუნებს -1 ს
+//string name = "landa";
+//int a = name.IndexOf("n");
+//Console.WriteLine(a);
+
+
+//// ამოწმებს რომელ ინდექსზე დგას , ბოლოდან ითვლის 
+//string name = "landa";
+//int a = name.LastIndexOf("d");
+//Console.WriteLine(a);
+
+////შესაბამის ინდექსზე ჩაამატებს
+//string name = "landa";
+//string a = name.Insert(0, "1");
+//Console.WriteLine(a);
+
+////შლის ინდექსიდან ყველაფერს
+//string name = "landa";
+//string a = name.Remove(2);
+//Console.WriteLine(a);
+
+
+////შლის ინდექსებს შორის
+//string name = "landa";
+//string a = name.Remove(0, 2);
+//Console.WriteLine(a);
+
+////შლის კონკრეტულ ინდექსამდე
+//string name = "landa";
+//string a = name.Substring(4);
+//Console.WriteLine(a);
+
+
+////ანაცვლებს
+//string name = "landa";
+//string a = name.Replace("n", "m");
+//Console.WriteLine(a);
+
+////სეპარატორი - გამყოფი
+////დაყო და ერეი მივიღე 
+//string name = "landa1, landa2, landa3";
+//string[] b = name.Split(',');
+//for(int i=0; i<b.Length; i++)
+//{
+//    Console.WriteLine(b[i]);
+//}
+
+
+////შლის 1 ინდექსიდან 2 ცალს
+//int[] number = { 1, 2, 3 };
+//Array.Clear(number, 1, 2);
+
+
+////შეატრიალებს
+//int[] number = { 1, 2, 3 };
+//Array.Reverse(number);
+//for (int i = 0; i < number.Length; i++)
+//{
+//    Console.WriteLine(number[i]);
+//}
+
+
+////პატარიდან დიდისკენ ალაგებს
+//int[] number = { 1, 2, 3,-2, 10 };
+//Array.Sort(number);
+//for (int i = 0; i < number.Length; i++)
+//{
+//    Console.WriteLine(number[i]);
+//}
+
+////მინიმალური და მაქსიმალური min max
+//int[] number = { 1, 2, 3, -2, 10 };
+//int a = number.Max();
+//Console.WriteLine(a);
+
+
+
+//// წაშლის
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//number.Clear();
+
+
+//// შეაბრუნებს
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//number.Reverse();
+
+
+
+////ლისტში ჩაამატებს მეორე ლისტის ელემენტებს
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//List<int> number1 = new List<int>() { 5, 6, 7, 8 };
+//number.AddRange(number1);
+
+
+
+//// შლის კონკრეტულ ელემენტს
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//number.Remove(3);
+
+//foreach (int newnumber in number)
+//{
+//    Console.WriteLine(newnumber);
+//}
+
+
+////წაშლის 1 ინდექსიდან 2 ელემენტს / კონკრეტული ინდექსიდან კონკრეტულ რაოდენობას
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//number.RemoveRange(1,2);
+
+//foreach (int newnumber in number)
+//{
+//    Console.WriteLine(newnumber);
+//}
+
+
+
+////ჩაამატებს 1 ინდექსიდან  ჩანაწერს  4 ს 
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//number.Insert(1,4);
+
+//foreach (int newnumber in number)
+//{
+//    Console.WriteLine(newnumber);
+//}
+
+
+// ჩააინსერტა მთლიანი ლისტი
+//List<int> number = new List<int>() { 1, 2, 3, 4 };
+//List<int> number1 = new List<int>() { 5,6,7 };
+//number.InsertRange(2,number1);
+
+//foreach (int newnumber in number)
+//{
+//    Console.WriteLine(newnumber);
+//}
+
+
+//მატრიცის დახატვა
+//int height = 3;
+//int widht = 4;
+
+//for (int i = 0; i < height; i++)
+//{
+//    for (int j = 0; j < widht; j++)
+//    {
+//        Console.Write('*');
+//    }
+
+//    Console.WriteLine();
+   
+//}
+
