1512d3c1a9fa1bcd739c1abed273fe4e3f0810f6
Landa Mindiashvili <lmindiashvili@credo.ge>
2024-12-04 20:23:58 +0400

    3

---
+++ learning/3 კვირა/3 kvira/Program.cs
@@ -0,0 +1,46 @@
+﻿////მეთოდები
+
+//// გვაქვს კონსოლ კლასი რომელშიც არის ვრაითლაინ მეთოდი რომელიც ბეჭდავს
+//Console.WriteLine("landa");
+
+
+////ლოკალური მეთოდი შევქმენი/  მეინ მეთოდში ვადეკლარირებ მეთოდს WriteLine
+//// ვოიდ ტიპის არის როცა უკან არაფერი არ ბრუნდება
+//void SayLanda ()
+//    {
+//    Console.WriteLine("Landa");
+//    }
+
+//SayLanda();
+
+//// აქ უნდა დაბრუნდეს
+
+//var rezult = SayLanda();
+//// დისქარდ სინტაქსი / მეთოდი გამოვიძახე მაგრამ ამის რეზალტი არ მაინტერესებს
+//_ = SayLanda();
+//int SayLanda()
+//{
+//    Console.WriteLine("Landa");
+//    return default;
+//    void SayMindiashvili()
+//    {
+//        Console.WriteLine("Mindiashvili");
+//    }
+//}
+
+
+
+////Console.WriteLine(SayLanda(1, 2));
+//_ = SayLanda(1, 2);
+//int SayLanda(int number1, int number2)
+//{
+//    Console.WriteLine("Hello Landa");
+//    SayMindiashvili();
+//    return number1 + number2;
+//    void SayMindiashvili()
+//    {
+//        Console.WriteLine("Mindiashvili");
+//    }
+//}
+
+// კლასები
